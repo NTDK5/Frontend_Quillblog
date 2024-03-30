49652c4e3df819006b5e290a586d989f67ca619c
NTDK <99682074+NTDK5@users.noreply.github.com>
2024-03-30 16:40:34 +0300

    Update apiSlice.js

--- src/slices/apiSlice.js
+++ src/slices/apiSlice.js
@@ -2,7 +2,7 @@
 import { fetchBaseQuery, createApi } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:5000",
+  baseUrl: "https://backend-quillblog.onrender.com/",
 });
 
 export const apiSlice = createApi({
