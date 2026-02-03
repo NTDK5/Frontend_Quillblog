206c360294eece58c8b7ac26eb2e4478b695efbc
NTDK <99682074+NTDK5@users.noreply.github.com>
2026-02-03 16:44:40 +0300

    Remove Header import from App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { ToastContainer } from "react-toastify";
-import Header from "./components/Header";
 import { Outlet } from "react-router-dom";
 import "react-toastify/dist/ReactToastify.css";
 
