f36507b5c4a6493659d711584c2a292286d656a6
NTDK <99682074+NTDK5@users.noreply.github.com>
2026-02-03 16:55:46 +0300

    Refactor LoginScreen component for improved readability

--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -4,21 +4,18 @@ import { useDispatch, useSelector } from "react-redux";
 import { useLoginMutation } from "../slices/usersApiSlice";
 import { setCredentials } from "../slices/authSlice";
 import { toast } from "react-toastify";
-import { FaSignInAlt, FaEnvelope, FaLock, FaUser } from "react-icons/fa";
-import signInImage from '../assets/signIn.jpg';
-import logo from '../assets/logo.png';
-import { FaPerson } from "react-icons/fa6";
-
-
+import { FaLock, FaUser } from "react-icons/fa";
+import signInImage from "../assets/signIn.jpg";
+import logo from "../assets/logo.png";
 
 const LoginScreen = () => {
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
+
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
   const [login, { isLoading }] = useLoginMutation();
-
   const { userInfo } = useSelector((state) => state.auth);
 
   useEffect(() => {
@@ -30,7 +27,7 @@ const LoginScreen = () => {
   const submitHandler = async (e) => {
     e.preventDefault();
     try {
-      const res = await login({ name, password}).unwrap();
+      const res = await login({ name, password }).unwrap();
       dispatch(setCredentials({ ...res }));
       navigate("/");
     } catch (err) {
@@ -40,42 +37,53 @@ const LoginScreen = () => {
 
   return (
     <div className="login_container">
-           <div className="sign_in_image">
-        <img src={signInImage} alt="signinimage" />
-        <div className="overlay"></div>
-      </div>
-      <div className="form_container">
-        <div className="form_content">
-        <img src={logo} width={70} height={70}></img>
-          <h1 className="welcome">Welcome Back to <span>Quillblog</span></h1>
-          <h6>sign in to your account bellow</h6>
-          <form onSubmit={submitHandler}>
-          <div className="form_group">
-  <FaUser className="input-icon" />
-  <input
-    type="name"
-    value={name}
-    onChange={(e) => setName(e.target.value)}
-    placeholder="Username"
-    required
-  />
-</div>
-<div className="form_group">
-  <FaLock className="input-icon" />
-  <input
-    type="password"
-    value={password}
-    placeholder="Password"
-    onChange={(e) => setPassword(e.target.value)}
-    required
-  />
-</div>
-        <button type="submit">Sign In</button>
-        <Link to="/register">Dosen't have an account, Sign up Now!</Link>
-      </form>
-        </div>
+      <div className="sign_in_image">
+        <img src={signInImage} alt="Sign in" />
+        <div className="overlay" />
       </div>
 
+      <div className="form_container">
+        <div className="form_content">
+          <img src={logo} alt="Quillblog logo" width={70} height={70} />
+
+          <h1 className="welcome">
+            Welcome Back to <span>Quillblog</span>
+          </h1>
+          <h6>Sign in to your account below</h6>
+
+          <form onSubmit={submitHandler}>
+            <div className="form_group">
+              <FaUser className="input-icon" />
+              <input
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                placeholder="Username"
+                required
+              />
+            </div>
+
+            <div className="form_group">
+              <FaLock className="input-icon" />
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Password"
+                required
+              />
+            </div>
+
+            <button type="submit" disabled={isLoading}>
+              {isLoading ? "Signing in..." : "Sign In"}
+            </button>
+
+            <Link to="/register">
+              Doesn't have an account? Sign up now!
+            </Link>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
